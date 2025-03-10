0d4a97f0ca492dce64328ad3f9d4d867148633a5
nishant <nishantjani942@gmail.com>
2025-03-10 17:03:48 +0530

    initial setup

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import Login from "./pages/Authentication/Login";
+import './assets/scss/themes.scss';
+
+function App() {
+  return (
+    <BrowserRouter>
+    
+        <Login />
+        
+      
+    </BrowserRouter>
+  );
+}
+
+
+
+
+export default App;
