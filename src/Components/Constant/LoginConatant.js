cbafd5c97fe1c20c3cd400926225a1034475485f
nishant <nishantdevops18@gmail.com>
2025-03-12 17:05:54 +0530

    login

--- src/Components/Constant/LoginConatant.js
+++ src/Components/Constant/LoginConatant.js
@@ -1,16 +1,4 @@
-// export const FORM_TEXTS = {
-//     EMAIL: {
-//       LABEL: "Email",
-//       PLACEHOLDER: "Enter email",
-//       ERROR_REQUIRED: "Email is required.",
-//       ERROR_INVALID: "Email should be in the correct format.",
-//     },
-//     PASSWORD: {
-//       LABEL: "Password",
-//       PLACEHOLDER: "Enter password",
-//       ERROR_REQUIRED: "Password is required.",
-//     },
-//   };
+
   
 
 export const Email = "Email"
